ee4d6562ed60c5e030644e9bb53d569e0c7269d8
Édouard WILLISSECK <edouard.willisseck@gmail.com>
2016-08-14 21:29:33 +0200

    Added a new map to display the 200m nearby radius around your position. This way when a rare pokemon appears on your nearby list you can open up PoGo-Spotted and see the radius in which the pokemon can be. It displays the previously reported spawnpoints so you know that the pokemon is most likely at one of these.

---
+++ static/js/nearby.js
@@ -0,0 +1,103 @@
+var map;
+var mapDiv = document.getElementById('map');
+var markerPlayer;
+var spawnMarkers = [];
+var coordinates;
+
+function initMap() {
+    var pos = {lat: 49.774, lng: 4.722};
+    
+    map = new google.maps.Map(mapDiv, {
+	center: pos,
+	zoom: 16
+    });
+
+    markerPlayer = new google.maps.Marker({
+	map: map,
+	icon: "../static/img/blue-dot.png",
+	draggable: true
+    });
+
+    circle = new google.maps.Circle({
+	fillColor: '#6666ff',
+	strokeColor: '#FF0000',
+	strokeOpacity: 0.8,
+	fillOpacity: 0.35,
+	map: map,
+	center: pos,
+	radius: 200
+    })
+
+    markerPlayer.setPosition(pos);
+    markerPlayer.bindTo("position", circle, "center");
+    
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+		lat: position.coords.latitude,
+		lng: position.coords.longitude
+            };
+
+	    map.setCenter(pos);
+	    markerPlayer.setPosition(pos);
+	    circle.setCenter(pos);
+        });
+    }
+
+    $.ajax({
+	type: "GET",
+	url: "/getCoord/",
+	success: function(answer) {
+	    coordinates = JSON.parse(answer);
+	    updateMarkers();
+	}
+    });
+}
+
+function updateMarkers() {
+    for (var key in coordinates) {
+	var enc_date = coordinates[key]["date"];
+	var coordObj = coordinates[key];
+
+	addMarker(coordObj, enc_date);
+    }
+}
+
+function addMarker(location, enc_date) {
+    var marker = new google.maps.Marker({
+	position: location,
+	map: map,
+	enc_date: enc_date
+    });
+
+    addInfoWindow(marker, enc_date);
+    spawnMarkers.push(marker);
+}
+
+function addInfoWindow(marker, enc_date) {
+    var infoWindow = new google.maps.InfoWindow({
+	content: enc_date
+    });
+
+    google.maps.event.
+	addListener(marker, 'click', (function(marker, info) {
+	    return function() {
+		info.open(map, marker);
+	    }
+	}) (marker, infoWindow));
+}
+
+$("#update_pos").click(function() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+		lat: position.coords.latitude,
+		lng: position.coords.longitude
+            };
+
+	    map.setCenter(pos);
+	    markerPlayer.setPosition(pos);
+        });
+    }
+});
